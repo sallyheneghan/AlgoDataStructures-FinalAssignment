d2e64d3dc8c9d444382b23db32576cc380798a4f
sallyheneghan <82091684+sallyheneghan@users.noreply.github.com>
2022-04-07 15:59:21 +0100

    Java classes for implementation of ShortestPath.java

--- FinalProject/ShortestPath.java
+++ FinalProject/ShortestPath.java
@@ -1,6 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ShortestPath {
 
 	
+	ShortestPath() {
+		//decision doc why Dijkstra? - as all costs are positive
+		// floud warshall - only if you need everywhere to everywhere - as we are entering two specific stops, FW is not optimal
+
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter start stop: " );
+		String start = scanner.next();
+		
+		System.out.print("Enter destination stop: ");
+		String destination = scanner.next();
+		
+		ShortestPath sp = new ShortestPath();
+		//ShortestPath();
+		
+		
+	}
 
 }
